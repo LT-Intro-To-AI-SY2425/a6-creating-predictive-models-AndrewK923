101e17ebe040b6ef4846d6fac57f8b271719efc7
Andrew Kozak <akozak1@cps.edu>
2024-12-19 10:18:29 -0600

    do writeups

--- part1-linear-regression/example3_blood_pressure.py
+++ part1-linear-regression/example3_blood_pressure.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LinearRegression
 '''
 Run the program and consider the following questions:
 1. Look at the data points on the graph. Do age and blood pressure appear to have a linear relationship?
@@ -9,18 +9,41 @@ Run the program and consider the following questions:
 '''
 
 data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
-x = data["Age"]
-y = data["Blood Pressure"]
+x = data["Age"].values
+y = data["Blood Pressure"].values 
+
+x = x.reshape(-1,1)
+
+model = LinearRegression().fit(x,y)
+
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x,y)
+
+x_predict = 42
+prediction = model.predict([[x_predict]])
+
 
 #sets the size of the graph
-plt.figure(figsize=(5,4))
+plt.figure(figsize=(6,4))
 
 #labels axes and creates scatterplot
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
 plt.xlabel("Age")
 plt.ylabel("Systolic Blood Pressure")
 plt.title("Systolic Blood Pressure by Age")
-plt.scatter(x, y)
 
-print("Pearson's Correlation: r = :", x.corr(y))
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit") 
 
+
+plt.legend()
 plt.show()
+
+
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction when x is {x_predict}: {prediction}")
+
+
+print(f"The predicted blood pressure for a {x_predict} year old is {prediction}") 
--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -11,17 +11,22 @@ from sklearn.model_selection import train_test_split
 data = pd.read_csv("part2-training-testing-data/blood_pressure_data.csv")
 x = data["Age"].values
 y = data["Blood Pressure"].values
-
+x = x.reshape(-1,1)
 # Create your training and testing datasets:
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2) 
 # Use reshape to turn the x values into 2D arrays:
-# xtrain = xtrain.reshape(-1,1)
+
 
 # Create the model
 
+model = LinearRegression().fit(xtrain, ytrain)
 # Find the coefficient, bias, and r squared values. 
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+rSquared = model.score(xtrain, ytrain)
 # Each should be a float and rounded to two decimal places. 
-
+print("Model's Linear Equation: y=",coef, "x+", intercept)
+print("R Squared value:", rSquared) 
 
 # Print out the linear equation and r squared value:
 
@@ -31,14 +36,33 @@ y = data["Blood Pressure"].values
 # reshape the xtest data into a 2D array
 
 # get the predicted y values for the xtest values - returns an array of the results
-
+predict = model.predict(xtest) 
 # round the value in the np array to 2 decimal places
-
+predict = np.around(predict, 2) 
 
 # Test the model by looping through all of the values in the xtest dataset
 print("\nTesting Linear Model with Testing Data:")
-
+for i in range(len(xtest)):
+    actual = ytest[i]
+    predicted_y = predict[i]
+    x_coord = xtest[i]
+    print("x value:" ,float(x_coord[0]), "Predicted y value:", predicted_y, "Actual y value:", actual)
+    
 
 '''
 **********CREATE A VISUAL OF THE RESULTS**********
 '''
+
+plt.figure(figsize=(5,4))
+plt.scatter(xtrain,ytrain, c="purple", label="Training Data")
+plt.scatter(xtest, ytest, c="blue", label="Testing Data")
+
+plt.scatter(xtest, predict, c="red", label="Predictions")
+
+plt.xlabel("Temperature ºF")
+plt.ylabel("Chirps per Minute")
+plt.title("Cricket Chirps by Temperature")
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
+
+plt.legend()
+plt.show() 
